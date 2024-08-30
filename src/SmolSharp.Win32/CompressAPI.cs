d429deff6e6cf0befaa050636875ea37f008d52e
phiyakanil <150898274+phiyakanil@users.noreply.github.com>
2024-08-30 18:40:03 +0530

    Add src/SmolSharp.Win32/CompressAPI.cs to data-compression

---
+++ src/SmolSharp.Win32/CompressAPI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SmolSharp.Win32
+{
+    internal static unsafe class CompressAPI
+    {
+        [SuppressGCTransition, DllImport("Cabinet")]
+        public static extern bool CreateDecompressor(
+            CompressAlgorithm algorithm,
+            nint allocationRoutines,
+            nint* decompressorHandle
+        );
+
+        [SuppressGCTransition, DllImport("Cabinet")]
+        public static extern bool Decompress(
+            nint decompressorHandle,
+            void* compressedData,
+            nint compressedDataSize,
+            void* uncompressedBuffer,
+            nint uncompressedBufferSize,
+            nint* uncompressedDataSize
+        );
+    }
+
+    public enum CompressAlgorithm : uint
+    {
+        MSZip = 2,
+        Xpress = 3,
+        XpressHuffman = 4,
+        LZMS = 5
+    }
+}
