2cd3ba84a25f9afc6507bb9b850179b3beed8d16
appmod-ai <appmod@techolution.com>
2024-09-13 09:12:27 +0000

    Cleaned directory in window-management-window-creation-21c88c

--- shaderpkg.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"./tools/shader_minifier.exe" -o minified.glsl --format text --smoothstep --aggressive-inlining %1
-"./src/ShaderCompressor/bin/x64/Release/ShaderCompressor.exe" minified.glsl compressed.txt
